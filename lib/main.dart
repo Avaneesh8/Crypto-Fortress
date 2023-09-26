98223c3366ad7f30ceb3e869d02f4bca19e92842
Avaneesh8 <maheshwari.avaneesh8@gmail.com>
2023-09-26 11:51:25 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:cryptofortress/View/io.dart';
 import 'package:cryptofortress/View/splash.dart';
+import 'package:cryptofortress/provider/auth_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(ChangeNotifierProvider(
+    create: (context) => AuthProvider(), // Initialize your AuthProvider
+    child: const MyApp(),
+  ),);
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
